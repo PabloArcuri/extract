952679f93ed9c41c81e35e0668900e4bfe18faae
Pablo <shynkaro@hotmail.com>
2023-03-16 13:22:55 -0300

    init

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pablo.pamf\Documents\Desenvolvimento\Controle_Nuvem\venv
+set VIRTUAL_ENV=C:\Users\pablo.pamf\OneDrive - Polícia Federal\Documentos\Desenvolvimento\extract\extract\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
